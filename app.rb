73bcad97e3165eac913dfcae3c1d668ba6e1f126
Gor <7495500@gmail.com>
2021-01-29 20:32:01 +0300

    Add log_something method

--- app.rb
+++ app.rb
@@ -2,6 +2,13 @@ class Logger
   def self.say_something
     puts 'Something'
   end
+
+  def self.log_something wat
+    f = File.open 'log.txt', 'a'
+    f.puts wat
+    f.close
+  end
 end
 
 Logger.say_something
+Logger.log_something 'wazzap'
