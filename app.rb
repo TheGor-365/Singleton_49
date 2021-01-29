827455084b28a24f945964120548b9b93a9c422e
Gor <7495500@gmail.com>
2021-01-29 21:01:01 +0300

    Add singleton lib

--- app.rb
+++ app.rb
@@ -1,30 +1,4 @@
-class Logger
-  def initialize
-    @f = File.open 'log.txt', 'a'
-  end
+require './logger'
 
-  @@x = nil
-
-  def self.instance
-    if @@x == nil
-      @@x = Logger.new
-    end
-
-    return @@x
-  end
-
-  def self.say_something
-    puts 'Something'
-  end
-
-  def log_something wat
-    @f.puts wat
-  end
-end
-
-Logger.say_something
 Logger.instance.log_something 'blabla'
 Logger.instance.log_something 'blabla2'
-
-logger = Logger.new
-logger.log_something 'hey'
